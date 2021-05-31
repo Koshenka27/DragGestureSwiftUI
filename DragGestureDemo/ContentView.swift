5ff7f7867d863e60ea4a9ab0eb7a48ea35f76d63
Logan Koshenka <koshenka27@gmail.com>
2021-05-30 21:24:45 -0400

    first commit :D

--- DragGestureDemo/ContentView.swift
+++ DragGestureDemo/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDragging = false
+    @State var position = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(isDragging ? .gray : .white)
+            
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 250, height: 200)
+                .offset(x: position.width, y: position.height)
+                .foregroundColor(.blue)
+                .animation(.linear)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            position = value.translation
+                            isDragging = true
+                        })
+                        .onEnded({ value in
+                            position = .zero
+                            isDragging = false
+                        })
+            )
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
