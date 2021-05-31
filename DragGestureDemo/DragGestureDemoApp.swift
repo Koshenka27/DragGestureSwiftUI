f3823c0a04c6f7aea9643ecc5448503db9987345
Logan Koshenka <koshenka27@gmail.com>
2021-05-30 20:53:08 -0400

    Initial Commit

---
+++ DragGestureDemo/DragGestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureDemoApp.swift
+//  DragGestureDemo
+//
+//  Created by Logan Koshenka on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
